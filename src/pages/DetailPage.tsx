357925cdc19971f8df24ecdd19733507f80981a6
Gabriele Bovolenta <101414963+gabriele-bovolenta@users.noreply.github.com>
2022-11-19 22:35:49 +0100

    sistemato

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -22,39 +22,40 @@ const DetailPage = () => {
         }
     }, [showId]) // con array vuoto, solo mount | se si specifica dei parametri viene eseguito al mount e al update
 
-    return <>{!!showDetail ?
-        <Card sx={{ maxWidth: 345, justifyContent: "center", alignItem: "center", margin: "2em", }} >
-            <CardMedia
-                component="img"
-                height="140"
-                image={showDetail?.image}
-                alt={showDetail?.title}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {showDetail?.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    <Interweave content={showDetail?.summary} />
+    return <>
+        {!!showDetail ?
+            <Card sx={{ maxWidth: 345, justifyContent: "center", alignItem: "center", margin: "2em", }} >
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={showDetail?.image}
+                    alt={showDetail?.title}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {showDetail?.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        <Interweave content={showDetail?.summary} />
 
-                </Typography>
-                <Typography>
-                    Genres: {showDetail?.genres}
-                </Typography>
-                <Typography>
-                    Rating: {showDetail?.avgRating}
-                </Typography>
-                <Typography>
-                    Start date: {showDetail?.startDate} End: {!showDetail?.endDate ? showDetail?.endDate : "Not finished"}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                Button doesn't work
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
-        : "Loading.."}
+                    </Typography>
+                    <Typography>
+                        Genres: {showDetail?.genres}
+                    </Typography>
+                    <Typography>
+                        Rating: {showDetail?.avgRating}
+                    </Typography>
+                    <Typography>
+                        Start date: {showDetail?.startDate} End: {!showDetail?.endDate ? showDetail?.endDate : "Not finished"}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    Button doesn't work
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+            : "Loading.."}
 
         <Button variant="contained" href="/">Back</Button>
     </>;
