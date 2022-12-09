c4e83158132dbcf7b83d3a48ebc67e95c6e5d0df
Gabriele Bovolenta <101414963+gabriele-bovolenta@users.noreply.github.com>
2022-12-09 15:31:08 +0100

    add ui for card movies

--- src/pages/Favourite.tsx
+++ src/pages/Favourite.tsx
@@ -5,13 +5,31 @@
 // remove movie dal databse removeMovieDatabase
 // controllare checkbox
 
-import { Grid } from "@mui/material";
+import { Checkbox, Grid } from "@mui/material";
 import { useEffect } from "react";
 import useFirebaseFavourite from "../Context/useFirebaseFavourite";
 import { Interweave } from "interweave";
 
 const Favourite = () => {
-  const [favourites] = useFirebaseFavourite();
+  const [favourites, addToFavourite, removeFromFavourite] = useFirebaseFavourite();
+
+  const label = {
+    inputprops: { "arial-label": "checkbox-favourites" },
+  };
+
+  const handleCheckbox = (
+    checked: boolean,
+    id: number,
+    title: string,
+    image?: string,
+    description?: string
+  ) => {
+    if (checked) {
+      removeFromFavourite(id);
+    } else {
+      addToFavourite(id, title, image, description);
+    }
+  };
 
   useEffect(() => {
     console.log(favourites);
@@ -24,10 +42,23 @@ const Favourite = () => {
       <Grid
         container
         justifyContent="center"
-        style={{ minHeight: "100vh", padding: "2em", width: "100%" }}
+        style={{ minHeight: "100vh", padding: "2em", width: "100%"}}
       >
         {favourites.map((el: any) => (
-          <div key={el.id} className="card">
+          <div key={el.id} className="card" style={{backgroundColor: 'black', height:"400px"}}>
+            <Checkbox
+                {...label}
+                onChange={(e) => {
+                  handleCheckbox(
+                    !!favourites.find((d) => el.id === d.id),
+                    el.id,
+                    el.title,
+                    el.image,
+                    el.description
+                  );
+                }}
+                checked={!!favourites.find((d) => el.id === d.id)}
+              />
             <figure className="card__thumb">
               <img src={el.image} alt="" className="card__image" />
               <figcaption className="card__caption">
@@ -36,6 +67,9 @@ const Favourite = () => {
                   className="card__snippet"
                   content={el.description}
                 />
+                {/* <Link to={el.id.toString()}>
+                  <Button variant="contained">Details</Button>
+                </Link> */}
                 <a href="./DetailPage.tsx" className="card__button">
                   Read more
                 </a>
@@ -47,5 +81,68 @@ const Favourite = () => {
     </>
   );
 };
+
+/* <Grid container justifyContent="center" style={{ height: "100vh" }}>
+      <Grid item style={{ padding: "2em", width: "100%" }}>
+        <Paper
+          component="form"
+          sx={{ display: "flex", alignItems: "center" }}
+          style={{ padding: "2em" }}
+          autoComplete="off"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <FormControl>
+            <InputBase
+              id="outlined-basic"
+              placeholder="Search by title..."
+              onChange={(e) => handleOnSearchChange(e.target.value)}
+              value={currentSearch.get("search") || ""}
+              autoFocus
+            />
+          </FormControl>
+          <FormControl>
+            <Button
+              disabled={isSearchButtonDisabled()}
+              onClick={handleOnSearch}
+            >
+              Search
+            </Button>
+          </FormControl>
+        </Paper>
+      </Grid>
+
+      <Grid item style={{ padding: "2em" }} />
+      {shows.map((el, i) => (
+        <Card
+          key={i}
+          sx={{ display: "flex", alignItems: "center", margin: "2em" }}
+        >
+          <Checkbox
+            {...label}
+            onChange={(e) => {
+              handleCheckbox(!!favourites.find((d) => el.id === d.id), el.id, el.title, el.image, el.description);
+            }}
+            checked={!!favourites.find((d) => el.id === d.id)}
+          />
+          <CardMedia
+            component="img"
+            height={140}
+            image={el.image}
+            alt={el.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              {el.title}
+            </Typography>
+          </CardContent>
+
+          <Link to={el.id.toString()}>
+            <Button variant="contained">Details</Button>
+          </Link>
+        </Card>
+      ))}
+    </Grid> */
 
 export default Favourite;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,19 +1,16 @@
 import {
   Button,
-  Card,
-  CardContent,
-  CardMedia,
   Checkbox,
   FormControl,
   Grid,
   InputBase,
   Paper,
-  Typography,
 } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import { getShowsBySearch, ShowType } from "../Api";
 import { Link, useSearchParams } from "react-router-dom";
 import useFirebaseFavourite from "../Context/useFirebaseFavourite";
+import { Interweave } from "interweave";
 
 const SearchPage = () => {
   const [currentSearch, setCurrentSearch] = useSearchParams();
@@ -27,7 +24,7 @@ const SearchPage = () => {
       setCurrentSearch({ search: query });
     },
     [setCurrentSearch]
-  ); 
+  );
 
   const isSearchButtonDisabled = () =>
     currentSearch.get("search")?.trim().length === 0;
@@ -42,7 +39,13 @@ const SearchPage = () => {
     inputprops: { "arial-label": "checkbox-favourites" },
   };
 
-  const handleCheckbox = (checked: boolean, id: number, title: string, image?: string, description?: string) => {
+  const handleCheckbox = (
+    checked: boolean,
+    id: number,
+    title: string,
+    image?: string,
+    description?: string
+  ) => {
     if (checked) {
       removeFromFavourite(id);
     } else {
@@ -63,68 +66,79 @@ const SearchPage = () => {
   }, [favourites]);
 
   return (
-    <Grid container justifyContent="center" style={{ height: "100vh" }}>
-      <Grid item style={{ padding: "2em", width: "100%" }}>
-        <Paper
-          component="form"
-          sx={{ display: "flex", alignItems: "center" }}
-          style={{ padding: "2em" }}
-          autoComplete="off"
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <FormControl>
-            <InputBase
-              id="outlined-basic"
-              placeholder="Search by title..."
-              onChange={(e) => handleOnSearchChange(e.target.value)}
-              value={currentSearch.get("search") || ""}
-              autoFocus
-            />
-          </FormControl>
-          <FormControl>
-            <Button
-              disabled={isSearchButtonDisabled()}
-              onClick={handleOnSearch}
-            >
-              Search
-            </Button>
-          </FormControl>
-        </Paper>
-      </Grid>
-
-      <Grid item style={{ padding: "2em" }} />
-      {shows.map((el, i) => (
-        <Card
-          key={i}
-          sx={{ display: "flex", alignItems: "center", margin: "2em" }}
-        >
-          <Checkbox
-            {...label}
-            onChange={(e) => {
-              handleCheckbox(!!favourites.find((d) => el.id === d.id), el.id, el.title, el.image, el.description);
+    <>
+      <Grid container justifyContent="center" style={{ height: "100vh" }}>
+        <Grid item style={{ padding: "2em", width: "100%" }}>
+          <Paper
+            component="form"
+            sx={{ display: "flex", alignItems: "center" }}
+            style={{ padding: "2em" }}
+            autoComplete="off"
+            onSubmit={(e) => {
+              e.preventDefault();
             }}
-            checked={!!favourites.find((d) => el.id === d.id)}
-          />
-          <CardMedia
-            component="img"
-            height={140}
-            image={el.image}
-            alt={el.title}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5">
-              {el.title}
-            </Typography>
-          </CardContent>
+          >
+            <FormControl>
+              <InputBase
+                id="outlined-basic"
+                placeholder="Search by title..."
+                onChange={(e) => handleOnSearchChange(e.target.value)}
+                value={currentSearch.get("search") || ""}
+                autoFocus
+              />
+            </FormControl>
+            <FormControl>
+              <Button
+                disabled={isSearchButtonDisabled()}
+                onClick={handleOnSearch}
+              >
+                Search
+              </Button>
+            </FormControl>
+          </Paper>
+        </Grid>
 
-          <Link to={el.id.toString()}>
-            <Button variant="contained">Details</Button>
-          </Link>
-        </Card>
-      ))}
-    </Grid>
+        <Grid
+          container
+          justifyContent="center"
+          style={{ minHeight: "100vh", padding: "2em", width: "100%" }}
+        >
+          {shows.map((el: any) => (
+            <div key={el.id} className="card" style={{backgroundColor: 'black'}}>
+              <Checkbox style={{backgroundColor: 'white'}}
+                {...label}
+                onChange={(e) => {
+                  handleCheckbox(
+                    !!favourites.find((d) => el.id === d.id),
+                    el.id,
+                    el.title,
+                    el.image,
+                    el.description
+                  );
+                }}
+                checked={!!favourites.find((d) => el.id === d.id)}
+              />
+              <figure className="card__thumb">
+                <img src={el.image} alt="" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">{el.title}</h2>
+                  <Interweave
+                    className="card__snippet"
+                    content={el.description}
+                  />
+                  <Link to={el.id.toString()}>
+                    <Button variant="contained">Details</Button>
+                  </Link>
+                  {/* <a href="./DetailPage.tsx" className="card__button">
+                    Read more
+                  </a> */}
+                </figcaption>
+              </figure>
+            </div>
+          ))}
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
